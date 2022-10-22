d11add1d7710ddcbf0b39258372bd21333814115
cmw <914136875@qq.com>
2022-10-22 16:12:45 +0800

    second commit

--- src/main/java/com/example/gitTest/hello.java
+++ src/main/java/com/example/gitTest/hello.java
@@ -3,5 +3,6 @@ package com.example.gitTest;
 public class hello {
     public static void main(String[] args) {
         System.out.println("heollo ,git ");
+        System.out.println("nb");
     }
 }
